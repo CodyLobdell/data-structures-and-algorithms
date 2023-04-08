f762d3e3d31b103321bb930ade6d5e90a2ec9594
Cody Lobdell <thecreator85@gmail.com>
2023-04-07 20:39:28 -0700

    added initial script for code challenge

---
+++ javascript/401-code-challenges/linked-list.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Require our linked list implementation
+const LinkedList = require('../linked-list');
+
+describe('Linked List', () => {
+  it('works', () => {
+    expect(true).toBeTruthy();
+  });
+});
