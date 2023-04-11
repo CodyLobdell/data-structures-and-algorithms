f89b3feab9782a4cd34da049796c3c3e901d49ed
Cody Lobdell <thecreator85@gmail.com>
2023-04-10 22:48:04 -0700

    added script for test

--- javascript/401-code-challenges/linked-list-insertions.test.js
+++ javascript/401-code-challenges/linked-list-insertions.test.js
@@ -1,110 +1,24 @@
 'use strict';
 
-const LinkedList = require('../index');
+const { LinkedList } = require('./LinkedList');
 
-describe('Testing the Linked List data structure', () => {
-  test('Can successfully instantiate an empty linked list', () => {
-    let newLinkedList = new LinkedList();
-    expect(newLinkedList.head).toBe(null);
-  });
+describe('LinkedList', () => {
+  describe('append', () => {
+    it('should add a new node to the end of an empty list', () => {
+      const list = new LinkedList();
+      list.append(1);
+      expect(list.head.value).toBe(1);
+      expect(list.tail.value).toBe(1);
+      expect(list.length).toBe(1);
+    });
 
-  test('Can properly insert into the linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.insert(1);
-    expect(newLinkedList.head.value).toEqual(1);
-  });
-
-  test('Can successfully add a node to the end of the linked list', () => {
-
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    expect(newLinkedList.head.value).toEqual(1);
-  });
-
-  test('Can successfully add multiple nodes to the end of a linked list', () => {
-
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    expect(newLinkedList.head.value).toEqual(1);
-    newLinkedList.append(2);
-    expect(newLinkedList.head.next.value).toEqual(2);
-  });
-
-
-  test('The head property will properly point to the first node in the linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    expect(newLinkedList.head.value).toBe(1);
-  });
-
-  test('Can properly insert multiple nodes into the linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.insert(1);
-    newLinkedList.insert(2);
-    expect(newLinkedList.head.value).toEqual(2);
-  });
-
-  test('Will return true when finding a value within the linked list that exists', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    let ans = newLinkedList.includes(1);
-    expect(ans).toBe(true);
-  });
-
-  test('Will return false when searching for a value in the linked list that does not exist', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    let ans = newLinkedList.includes(2);
-    expect(ans).toBe(false);
-  });
-
-  test('Can properly return a collection of all the values that exist in the linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    newLinkedList.append(2);
-    let str = newLinkedList.toString();
-    expect(str).toEqual('{ 1 } -> { 2 } -> NULL');
-  });
-
-  test('Can successfully insert a node before a node located i the middle of a linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    newLinkedList.append(2);
-    newLinkedList.append(3);
-    newLinkedList.append(4);
-    newLinkedList.append(5);
-    newLinkedList.insertBefore(3, 9);
-    let str = newLinkedList.toString();
-    expect(str).toEqual('{ 1 } -> { 2 } -> { 9 } -> { 3 } -> { 4 } -> { 5 } -> NULL');
-  });
-
-  test('Can successfully insert a node before the first node of a linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    newLinkedList.append(2);
-    newLinkedList.insertBefore(1, 9);
-    let str = newLinkedList.toString();
-    expect(str).toEqual('{ 9 } -> { 1 } -> { 2 } -> NULL');
-  });
-
-  test('Can successfully insert after a node in the middle of the linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    newLinkedList.append(2);
-    newLinkedList.append(3);
-    newLinkedList.append(4);
-    newLinkedList.insertAfter(3, 9);
-    let str = newLinkedList.toString();
-    expect(str).toEqual('{ 1 } -> { 2 } -> { 3 } -> { 9 } -> { 4 } -> NULL');
-  });
-
-  test('Can successfully insert a node after the last node of the linked list', () => {
-    let newLinkedList = new LinkedList();
-    newLinkedList.append(1);
-    newLinkedList.append(2);
-    newLinkedList.insertAfter(2, 9);
-    let str = newLinkedList.toString();
-    expect(str).toEqual('{ 1 } -> { 2 } -> { 9 } -> NULL');
+    it('should add a new node to the end of a non-empty list', () => {
+      const list = new LinkedList();
+      list.append(1);
+      list.append(2);
+      expect(list.head.value).toBe(1);
+      expect(list.tail.value).toBe(2);
+      expect(list.length).toBe(2);
+    });
   });
 });
-
